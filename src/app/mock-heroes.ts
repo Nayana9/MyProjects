5289ed01b47183d9640b0862cfada3b82e68b75d
Nayana Santhosh <NayanaS@CCMT.local>
2017-07-06 16:20:28 +0530

    My first git practical

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+{ id: 11, name: 'Praful' },
+ { id: 12, name: 'Pallavi' },
+ { id: 13, name: 'Nayana' },
+ { id: 14, name: 'Hemant' },
+ { id: 15, name: 'Shredha' },
+ { id: 16, name: 'Shubhangi' },
+ { id: 17, name: 'Radhika' },
+ { id: 18, name: 'Bala' },
+ { id: 19, name: 'Vikrant' },
+ { id: 20, name: 'Remith' }
+];
